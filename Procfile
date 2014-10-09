c4e96c937863f048d26001e181fc4d10dc8f25a7
aristekrat <aristekrat@gmail.com>
2014-10-09 10:40:11 -0600

    Emails, other things

--- Procfile
+++ Procfile
@@ -1,2 +1,4 @@
-web: gunicorn app:app --log-file=-
-scrape: python exec_scripts.py
+web: python3 app.py
+scrape: python3 ./scraper/scrape.py
+create_db: python3 db_create.py
+add_venues: python3 denver_venues.py
--- app.py
+++ app.py
@@ -1,18 +1,49 @@
 from flask import Flask, jsonify, make_response, request, current_app
 from flask.ext.sqlalchemy import SQLAlchemy
-from datetime import timedelta
-from functools import update_wrapper
 from flask.ext.cors import CORS
-
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
 
 app = Flask(__name__)
-# One of the simplest configurations. Exposes all resources matching /api/* to
-# CORS and allows the Content-Type header, which is necessary to POST JSON
-# cross origin.
 CORS(app, resources='/*', headers='Content-Type')
 app.config.from_object('config')
-db = SQLAlchemy(app)
-import routes
 
+db = SQLAlchemy(app)
+
+import models
+
+#Begin Routes - for whatever reason the routes fail with No 'Access-Control-Allow-Origin' errors when placed outside of app.py
+#This is true even of the routes logic. It's baffling.
+def prep_concert_response(results):
+	response = []
+	for result in results:
+		t = models.Venue.query.get(result.concertVenueId)
+		temp_fix = str(result.showDate) #sending in the object straight causes a cross origin error for some damn reason.
+		x = {"date": temp_fix, "band": result.band, "venue": t.venueName}
+		response.append(x)
+	return response
+
+@app.route('/')
+def default():
+    return "You've reached Hipster Tron's data server. Unfortunately, there's nothing for people to do here, just scripts. Sowwy :-("
+
+@app.route('/getConcerts', methods=['GET', 'OPTIONS'])
+def returnConcerts():
+	concerts = models.Denver_Concerts.query.order_by(models.Denver_Concerts.showDate)
+	response = prep_concert_response(concerts)
+	return jsonify(concertListings=response)
+
+@app.route('/sendEmail', methods=['POST'])
+def sendEmail():
+	userEmail = request.get_json()
+	newEmail = models.Emails(email = userEmail)
+	db.session.add(newEmail)
+	db.session.commit()
+	return "Complete"
+
+# Server Start
 if __name__ == '__main__':
-    app.run()
+  http_server = HTTPServer(WSGIContainer(app))
+  http_server.listen(8000)
+  IOLoop.instance().start()
